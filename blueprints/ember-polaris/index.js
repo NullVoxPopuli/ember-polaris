1d8927c3d821a47470c8edb5dde6eacfba03fca7
Andy Pye <apye@sweettoothhq.com>
2018-03-29 13:21:00 +0300

    Try adding svg-jar options to host app's  file

--- blueprints/ember-polaris/index.js
+++ blueprints/ember-polaris/index.js
@@ -11,6 +11,32 @@ module.exports = {
   afterInstall: function(/* options */) {
     return this.addPackagesToProject([
       { name: 'ember-cli-sass', target: 'latest' }
-    ]);
+    ]).then(function() {
+      var BuildConfigEditor = require('ember-cli-build-config-editor.js');
+      var fs = require('fs');
+
+      // Specify 'utf-8' to force it to be a string instead of a buffer
+      var emberCliBuildPath = './ember-cli-build.js';
+      var source = fs.readFileSync(emberCliBuildPath, 'utf-8');
+
+      var build = new BuildConfigEditor(source);
+
+      build.edit('svgJar', {
+        // TODO check the advantages of using a `symbol` strategy
+        strategy: 'inline',
+        inline: {
+          stripPath: false,
+          optimizer: {
+            removeTitle: true,
+            removeDimensions: true,
+          },
+          sourceDirs: [
+            'public/images/svg',
+          ],
+        }
+      });
+
+      fs.writeFileSync(emberCliBuildPath, build.code());
+    });
   }
 };
